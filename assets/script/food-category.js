810772a601ed7e4f3869b6cd7319489072359b4e
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-06-08 16:48:56 +0330

    add active class

--- assets/script/food-category.js
+++ assets/script/food-category.js
@@ -4,8 +4,23 @@ const swiperWrapper = $("<div>").addClass("swiper-wrapper");
 // Create the swiper slide elements
 const swiperSlides = ["پیتزا", "برگر", "مرغ سوخاری", "سالاد", "پیش غذا"];
 
-swiperSlides.forEach(function (slideText) {
-  const swiperSlide = $("<span>").addClass("swiper-slide").text(slideText);
+swiperSlides.forEach(function (slideText, index) {
+  const swiperSlide = $("<span>")
+    .addClass("swiper-slide")
+    .text(slideText)
+    .click(function () {
+      // Remove active class from all other slides
+      $(".swiper-slide").removeClass("active");
+
+      // Add active class to the clicked slide
+      $(this).addClass("active");
+    });
+
+  // Set the first slide as active by default
+  if (index === 0) {
+    swiperSlide.addClass("active");
+  }
+
   swiperWrapper.append(swiperSlide);
 });
 
