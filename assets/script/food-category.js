6ee129913f6d9a7dba73676e55e99f50f0cb6950
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-06-08 17:47:30 +0330

    fake commit

--- assets/script/food-category.js
+++ assets/script/food-category.js
@@ -1,3 +1,23 @@
+// Function to handle slide click event
+function handleSlideClick() {
+  const slide = $(this);
+  const index = slide.index();
+
+  // Remove active class from all other slides
+  $(".swiper-slide").removeClass("active");
+
+  // Add active class to the clicked slide
+  slide.addClass("active");
+
+  // Update the active index text
+  const activeText = swiperSlides[index].text;
+  $(".second-template__food h2").text(activeText);
+
+  // Update the image source based on the active index
+  const activeImage = swiperSlides[index].image;
+  $(".second-template__food img").attr("src", activeImage);
+}
+
 // Create the swiper wrapper element
 const swiperWrapper = $("<div>").addClass("swiper-wrapper");
 
@@ -5,28 +25,16 @@ const swiperWrapper = $("<div>").addClass("swiper-wrapper");
 const swiperSlides = [
   { text: "پیتزا", image: "assets/images/pitza.png" },
   { text: "برگر", image: "assets/images/burgur.jpg" },
-  { text: "مرغ سوخاری", image: "assets/images/chicken.png" },
-  { text: "سالاد", image: "assets/images/salad.png" },
-  { text: "پیش غذا", image: "assets/images/appetizer.png" },
+  { text: "مرغ سوخاری", image: "assets/images/pitza.png" },
+  { text: "سالاد", image: "assets/images/burgur.jpg" },
+  { text: "پیش غذا", image: "assets/images/pitza.png" },
 ];
 
 swiperSlides.forEach(function (slide, index) {
   const swiperSlide = $("<span>")
     .addClass("swiper-slide")
     .text(slide.text)
-    .click(function () {
-      // Remove active class from all other slides
-      $(".swiper-slide").removeClass("active");
-
-      // Add active class to the clicked slide
-      $(this).addClass("active");
-
-      // Update the active index text
-      $(".second-template__food h2").text(slide.text);
-
-      // Update the image source based on the active index
-      $(".second-template__food img").attr("src", slide.image);
-    });
+    .click(handleSlideClick);
 
   // Set the first slide as active by default
   if (index === 0) {
