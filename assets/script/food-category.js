c36ca1c4603377f6e34ec0a69b0974339362681d
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-06-08 17:42:49 +0330

    update code

--- assets/script/food-category.js
+++ assets/script/food-category.js
@@ -2,12 +2,18 @@
 const swiperWrapper = $("<div>").addClass("swiper-wrapper");
 
 // Create the swiper slide elements
-const swiperSlides = ["پیتزا", "برگر", "مرغ سوخاری", "سالاد", "پیش غذا"];
+const swiperSlides = [
+  { text: "پیتزا", image: "assets/images/pitza.png" },
+  { text: "برگر", image: "assets/images/burgur.jpg" },
+  { text: "مرغ سوخاری", image: "assets/images/chicken.png" },
+  { text: "سالاد", image: "assets/images/salad.png" },
+  { text: "پیش غذا", image: "assets/images/appetizer.png" },
+];
 
-swiperSlides.forEach(function (slideText, index) {
+swiperSlides.forEach(function (slide, index) {
   const swiperSlide = $("<span>")
     .addClass("swiper-slide")
-    .text(slideText)
+    .text(slide.text)
     .click(function () {
       // Remove active class from all other slides
       $(".swiper-slide").removeClass("active");
@@ -16,15 +22,19 @@ swiperSlides.forEach(function (slideText, index) {
       $(this).addClass("active");
 
       // Update the active index text
-      $(".second-template__food h2").text(slideText);
+      $(".second-template__food h2").text(slide.text);
+
+      // Update the image source based on the active index
+      $(".second-template__food img").attr("src", slide.image);
     });
 
   // Set the first slide as active by default
   if (index === 0) {
     swiperSlide.addClass("active");
 
-    // Set the initial active index text
-    $(".second-template__food h2").text(slideText);
+    // Set the initial active index text and image
+    $(".second-template__food h2").text(slide.text);
+    $(".second-template__food img").attr("src", slide.image);
   }
 
   swiperWrapper.append(swiperSlide);
