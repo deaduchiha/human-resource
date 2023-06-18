4a18f7e1053c37152e1a94879c9fe4e263d6f339
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-06-18 17:07:10 +0330

    fix bugs and update video and popup images

--- common/script/script.js
+++ common/script/script.js
@@ -38,46 +38,6 @@ function updatePopupContent(foodItem) {
   );
 }
 
-// Function to handle "next" arrow click
-function showNext() {
-  // Increment the current index
-  currentIndex++;
-
-  // Get the filtered food list
-  const filteredFoodList = getFilteredFoodList();
-
-  // Check if the index exceeds the length of the filtered food list
-  if (currentIndex >= filteredFoodList.length) {
-    currentIndex = 0; // Reset to the beginning
-  }
-
-  // Get the food item based on the current index
-  const foodItem = filteredFoodList[currentIndex];
-
-  // Update the popup content with the new food item details
-  updatePopupContent(foodItem);
-}
-
-// Function to handle "prev" arrow click
-function showPrev() {
-  // Decrement the current index
-  currentIndex--;
-
-  // Get the filtered food list
-  const filteredFoodList = getFilteredFoodList();
-
-  // Check if the index is less than 0
-  if (currentIndex < 0) {
-    currentIndex = filteredFoodList.length - 1; // Wrap around to the end
-  }
-
-  // Get the food item based on the current index
-  const foodItem = filteredFoodList[currentIndex];
-
-  // Update the popup content with the new food item details
-  updatePopupContent(foodItem);
-}
-
 // Function to get the filtered food list based on the current category
 function getFilteredFoodList() {
   return foodItems.filter(
--- templates/2. Yakh/assets/script/video.js
+++ templates/2. Yakh/assets/script/video.js
@@ -1,32 +1,20 @@
-function openFullscreen() {
-  const videoSrc = "../../common/video/pizza.mp4"; // New video source
-  const videoType = "video/mp4"; // New video type
-
-  const $video = $("#myvideo");
-  $video.empty(); // Clear existing source elements
-
-  const $source = $("<source>");
-  $source.attr("src", videoSrc);
-  $source.attr("type", videoType);
-
-  $video.append($source);
-
-  const elem = document.getElementById("myvideo");
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) {
-    /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) {
-    /* IE11 */
-    elem.msRequestFullscreen();
-  }
-}
-
 $(document).ready(function () {
   $(".food-details__video").click(function () {
-    $(".food-details video").css("display", "inherit");
-    $(".food-details video").on("ended webkitendfullscreen", function () {
+    const videoSrc = "../../common/video/pizza.mp4"; // New video source
+    const videoType = "video/mp4"; // New video type
+
+    const $video = $("#myvideo");
+    $video.empty(); // Clear existing source elements
+
+    const $source = $("<source>");
+    $source.attr("src", videoSrc);
+    $source.attr("type", videoType);
+
+    $video.append($source);
+    $video.css("display", "inherit");
+    $video.get(0).play(); // Start playing the video
+
+    $video.on("ended", function () {
       $(this).css("display", "none");
     });
   });
