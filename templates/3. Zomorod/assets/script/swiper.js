b45a20d953f61fa92cf0ebfd8790619412077b60
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-06-20 16:15:16 +0330

    create a pagination

--- templates/3. Zomorod/assets/script/swiper.js
+++ templates/3. Zomorod/assets/script/swiper.js
@@ -2,4 +2,8 @@ const swiper = new Swiper(".zomorod__slider", {
   initialSlide: 1,
   watchSlidesProgress: true,
   slidesPerView: "auto",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
