c2d1b55c1ffd0f9aeac51bc5682d676b5afbf397
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-06-24 13:14:13 +0330

    fix sorme bug

--- templates/4. Sorme/assets/script/category.js
+++ templates/4. Sorme/assets/script/category.js
@@ -3,6 +3,7 @@ import { foodCategory, foodList } from "./data.js";
 $(document).ready(function () {
   const swiperWrapper = $(".categorySwiper .swiper-wrapper");
   const foodSwiperWrapper = $(".foodSwiper .swiper-wrapper");
+  let foodSwiper = null; // Declare the foodSwiper variable
 
   // Generate HTML for each category
   foodCategory.forEach((category) => {
@@ -63,14 +64,101 @@ $(document).ready(function () {
       foodNames.append(foodTitle, foodEnglishTitle);
       foodSlide.append(foodImage, foodNames, foodPrice);
       foodSwiperWrapper.append(foodSlide);
+
+      // Add click event handler to the food slide
+      foodSlide.on("click", function () {
+        const foodId = food.id; // Use the ID or a unique identifier of the food item
+        const clickedFood = foodList.find((item) => item.id === foodId);
+
+        // Show the details of the clicked food item
+        console.log(clickedFood);
+        // Replace the above line with your code to show the details of the clicked food item
+
+        // Open the popup with the details of the clicked food item
+        openPopup(clickedFood);
+      });
     });
 
-    // Initialize the food Swiper component
-    const foodSwiper = new Swiper(".foodSwiper", {
+    // Function to open the popup with food details
+    function openPopup(food) {
+      // Update the popup content with the details of the clicked food item
+      $("#popup .popup-header p").text(food.title);
+      $("#popup .popup-header span").text(food.englishTitle);
+      $("#popup .popup-information .price").text(food.price + " تومان");
+      $("#popup .popup-information .description span").text(food.description);
+
+      // Set the src attribute of the img tag inside popup-image__holder
+      const popupImageHolder = $(".popup-image__holder");
+      const activeImageSrc = food.slideImage[0];
+      popupImageHolder.find("img").attr("src", activeImageSrc);
+
+      // Create swiper slides dynamically
+      const swiperWrapper = $(".verticalSwiper .swiper-wrapper");
+      swiperWrapper.empty(); // Clear existing slides
+
+      // Loop through slideImage array and create swiper-slide elements
+      for (let i = 0; i < food.slideImage.length; i++) {
+        const slideImage = food.slideImage[i];
+        const swiperSlide = $("<div>").addClass("swiper-slide");
+
+        // Create img tag and set the source from slideImage array
+        const img = $("<img>")
+          .attr("src", slideImage)
+          .attr("alt", "")
+          .data("index", i); // Set the data-index attribute with the slide index
+
+        // Add click event handler to the image
+        img.on("click", function () {
+          const clickedIndex = $(this).data("index");
+
+          // Update the active index and set the corresponding image src
+          const activeImageSrc = food.slideImage[clickedIndex];
+          popupImageHolder.find("img").attr("src", activeImageSrc);
+
+          // Activate the corresponding swiper-slide
+          verticalSwiper.slideTo(clickedIndex);
+        });
+
+        swiperSlide.append(img);
+        swiperWrapper.append(swiperSlide);
+      }
+
+      // Initialize the swiper after dynamically creating the slides
+      const verticalSwiper = new Swiper(".verticalSwiper", {
+        direction: "vertical",
+        slidesPerView: 3,
+        spaceBetween: 35,
+        initialSlide: 0,
+        navigation: {
+          nextEl: ".nextEl",
+        },
+      });
+
+      // Show the popup
+      $("#popup").fadeIn();
+    }
+
+    // Destroy the previous foodSwiper instance if it exists
+    if (foodSwiper) {
+      foodSwiper.destroy();
+    }
+
+    // Initialize the foodSwiper component
+    foodSwiper = new Swiper(".foodSwiper", {
       slidesPerView: "auto",
       centeredSlides: true,
       watchSlidesProgress: true,
       initialSlide: 1,
+    });
+
+    // Add click event handler to each food slide
+    const foodSlides = $(".foodSwiper .swiper-slide");
+    foodSlides.on("click", function () {
+      const foodId = $(this).data("food-id");
+      const selectedFood = foodList.find((food) => food.id === foodId);
+
+      // Display the details of the selected food item
+      console.log(selectedFood); // Replace this line with your code to show the details
     });
   });
 
@@ -91,11 +179,17 @@ $(document).ready(function () {
       return foodList;
     } else {
       // Filter food items based on the selected category ID
-      return foodList.filter(
-        (food) =>
-          food.category ===
-          foodCategory.find((category) => category.id === categoryId).text
+      const selectedCategory = foodCategory.find(
+        (category) => category.id === categoryId
       );
+      if (selectedCategory) {
+        return foodList.filter(
+          (food) => food.category === selectedCategory.text
+        );
+      } else {
+        // Invalid category ID, return an empty array
+        return [];
+      }
     }
   }
 });
--- templates/4. Sorme/assets/script/popup.js
+++ templates/4. Sorme/assets/script/popup.js
@@ -7,63 +7,72 @@ $(document).ready(function () {
     const foodIndex = $(this).index();
     const food = foodList[foodIndex];
 
-    $("#popup .popup-header p").text(food.title);
-    $("#popup .popup-header span").text(food.englishTitle);
-    $("#popup .popup-information .price").text(food.price + " تومان");
-    $("#popup .popup-information .description span").text(food.description);
-
-    // Set the src attribute of the img tag inside popup-image__holder
-    const popupImageHolder = $(".popup-image__holder");
-    const activeImageSrc = food.slideImage[activeIndex];
-    popupImageHolder.find("img").attr("src", activeImageSrc);
-
-    // Create swiper slides dynamically
-    const swiperWrapper = $(".verticalSwiper .swiper-wrapper");
-    swiperWrapper.empty(); // Clear existing slides
-
-    // Loop through slideImage array and create swiper-slide elements
-    for (let i = 0; i < food.slideImage.length; i++) {
-      const slideImage = food.slideImage[i];
-      const swiperSlide = $("<div>").addClass("swiper-slide");
-
-      // Create img tag and set the source from slideImage array
-      const img = $("<img>")
-        .attr("src", slideImage)
-        .attr("alt", "")
-        .data("index", i); // Set the data-index attribute with the slide index
-
-      // Add click event handler to the image
-      img.on("click", function () {
-        const clickedIndex = $(this).data("index");
-
-        // Update the active index and set the corresponding image src
-        activeIndex = clickedIndex;
-        const activeImageSrc = food.slideImage[activeIndex];
-        popupImageHolder.find("img").attr("src", activeImageSrc);
-
-        // Activate the corresponding swiper-slide
-        verticalSwiper.slideTo(clickedIndex);
-      });
-
-      swiperSlide.append(img);
-      swiperWrapper.append(swiperSlide);
-    }
-
-    // Initialize the swiper after dynamically creating the slides
-    const verticalSwiper = new Swiper(".verticalSwiper", {
-      direction: "vertical",
-      slidesPerView: 3,
-      spaceBetween: 35,
-      initialSlide: 0, // Set the initial slide to the active index
-      navigation: {
-        nextEl: ".nextEl",
-      },
-    });
-
-    $("#popup").fadeIn();
+    openPopup(food);
   });
 
   $(document).on("click", "#closeBtn", function () {
     $("#popup").fadeOut();
   });
+});
+
+// Function to open the popup with food details
+function openPopup(food) {
+  $("#popup .popup-header p").text(food.title);
+  $("#popup .popup-header span").text(food.englishTitle);
+  $("#popup .popup-information .price").text(food.price + " تومان");
+  $("#popup .popup-information .description span").text(food.description);
+
+  // Set the src attribute of the img tag inside popup-image__holder
+  const popupImageHolder = $(".popup-image__holder");
+  const activeImageSrc = food.slideImage[activeIndex];
+  popupImageHolder.find("img").attr("src", activeImageSrc);
+
+  // Create swiper slides dynamically
+  const swiperWrapper = $(".verticalSwiper .swiper-wrapper");
+  swiperWrapper.empty(); // Clear existing slides
+
+  // Loop through slideImage array and create swiper-slide elements
+  for (let i = 0; i < food.slideImage.length; i++) {
+    const slideImage = food.slideImage[i];
+    const swiperSlide = $("<div>").addClass("swiper-slide");
+
+    // Create img tag and set the source from slideImage array
+    const img = $("<img>")
+      .attr("src", slideImage)
+      .attr("alt", "")
+      .data("index", i); // Set the data-index attribute with the slide index
+
+    // Add click event handler to the image
+    img.on("click", function () {
+      const clickedIndex = $(this).data("index");
+
+      // Update the active index and set the corresponding image src
+      activeIndex = clickedIndex;
+      const activeImageSrc = food.slideImage[activeIndex];
+      popupImageHolder.find("img").attr("src", activeImageSrc);
+
+      // Activate the corresponding swiper-slide
+      verticalSwiper.slideTo(clickedIndex);
+    });
+
+    swiperSlide.append(img);
+    swiperWrapper.append(swiperSlide);
+  }
+
+  // Initialize the swiper after dynamically creating the slides
+  const verticalSwiper = new Swiper(".verticalSwiper", {
+    direction: "vertical",
+    slidesPerView: 3,
+    spaceBetween: 35,
+    initialSlide: 0, // Set the initial slide to the active index
+    navigation: {
+      nextEl: ".nextEl",
+    },
+  });
+
+  $("#popup").fadeIn();
+}
+
+$(document).on("click", "#closeBtn", function () {
+  $("#popup").fadeOut();
 });
