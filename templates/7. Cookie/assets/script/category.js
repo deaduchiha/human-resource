61c098844d60ff6b1e55711d3edd4d2ad5bf7da2
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-08-03 15:17:06 +0330

    click handler for categories

--- templates/7. Cookie/assets/script/category.js
+++ templates/7. Cookie/assets/script/category.js
@@ -10,13 +10,31 @@ $(document).ready(() => {
   const swiperWrapper = $(".categories .swiper-wrapper");
 
   // show categories
-  foodCategory.map((data) => {
+  foodCategory.map((data, index) => {
     const category = $("<div>").addClass("swiper-slide");
     category.attr("category-id", data.id);
+    const iconHolder = $("<div>").addClass("icon-holder");
     const icon = $("<svg>").attr("stroke", "currentColor").load(data.icon);
+    iconHolder.append(icon);
     const categoryTitle = $("<span>").text(data.text);
 
-    category.append(icon, categoryTitle);
+    category.append(iconHolder, categoryTitle);
     swiperWrapper.append(category);
+
+    if (index === 0) {
+      category.addClass("active-category");
+    }
+
+    category.on("click", () => handleCategoryClick(category));
   });
+
+  // handle click event on category
+  const handleCategoryClick = (clickedCategory) => {
+    // Remove the "active-category" class from all categories
+    const categories = $(".categories .swiper-slide");
+    categories.removeClass("active-category");
+
+    // Add the "active-category" class to the clicked category
+    clickedCategory.addClass("active-category");
+  };
 });
