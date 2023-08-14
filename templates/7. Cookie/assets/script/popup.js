54804f9cdccebbf41b08f89813af80645ec5cec8
Alireza <alireza.nikzdd@gmail.com>
2023-08-14 12:59:55 +0330

    handle to showing popup

--- templates/7. Cookie/assets/script/popup.js
+++ templates/7. Cookie/assets/script/popup.js
@@ -1,17 +1,18 @@
 import { foodList } from "./data.js";
-import { openPopup, destroyPopupSwiper } from "./utils.js";
+import { openPopup } from "./utils.js";
+
+const popupWrapper = $(".popup__info");
 
 $(document).ready(function () {
   $(document).on("click", "#openBtn", function () {
     const foodId = $(this).data("food-id"); // Get the data-food-id attribute from the clicked food item
     const food = foodList.find((item) => item.id === foodId); // Find the corresponding food object
 
-  
-    openPopup(food);
+    openPopup(food, popupWrapper);
   });
 
   $(document).on("click", "#closeBtn", function () {
     $("#popup").fadeOut();
-    destroyPopupSwiper(); // Destroy the Swiper instance when closing the popup
+    popupWrapper.empty();
   });
 });
--- templates/7. Cookie/assets/script/utils.js
+++ templates/7. Cookie/assets/script/utils.js
@@ -1,34 +1,16 @@
 export let popupSwiper; // Declare a global variable to hold the Swiper instance
 
-export function openPopup(food) {
-  // Update the popup content with the details of the clicked food item
-  $("#popup .food-name p").text(food.title);
-  $("#popup .food-name span").text(food.englishTitle);
-  $("#popup .food-information span").text(food.description);
-  $("#popup .food-price span").text(food.price + " تومان");
+export function openPopup(food, popupWrapper) {
+  const title = $("<p>").addClass("popup-food_title").text(food.title);
+  const englishTitle = $("<p>")
+    .addClass("popup-food_english-title")
+    .text(food.englishTitle);
+  const description = $("<span>")
+    .addClass("popup-food_description")
+    .text(food.description);
 
-  // Create swiper slides dynamically
-  const swiperWrapper = $(".popupSlider .swiper-wrapper");
-  swiperWrapper.empty(); // Clear existing slides
-
- 
-
-  // Initialize the swiper after dynamically creating the slides
-  popupSwiper = new Swiper(".popupSlider", {
-    slidesPerView: 1,
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
+  popupWrapper.append(title, englishTitle, description);
 
   // Show the popup
   $("#popup").fadeIn();
-}
-
-// Function to destroy the Swiper instance
-export function destroyPopupSwiper() {
-  if (popupSwiper) {
-    popupSwiper.destroy();
-    popupSwiper = null;
-  }
 }
