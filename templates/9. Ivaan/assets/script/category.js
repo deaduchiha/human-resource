b5c57c9357e52b416602177aa8294c3683e1ba96
Alireza Nikzad <alireza.nikzdd@gmail.com>
2023-08-26 13:12:52 +0330

    add sub category

--- templates/9. Ivaan/assets/script/category.js
+++ templates/9. Ivaan/assets/script/category.js
@@ -2,18 +2,50 @@ import { categories, foodsData } from "./data.js";
 
 $(document).ready(() => {
   // categories slider
-  let mySwiper = new Swiper(".categories", {
+  new Swiper(".categories", {
     slidesPerView: "auto",
     spaceBetween: 22,
   });
 
+  // sub categories slider
+  new Swiper(".subCategory", {
+    slidesPerView: "auto",
+    spaceBetween: 15,
+  });
+
   const swiperWrapper = $(".categories .swiper-wrapper");
+  const subCategoriesSwiperWrapper = $(".subCategory .swiper-wrapper");
   // const foodsWrapper = $(".foods");
 
   categories.map((data, index) => {
     const swiperSlide = $("<div>").addClass("swiper-slide");
     const category = $("<span>").text(data.category);
+
+    // active first category
     index === 0 && swiperSlide.addClass("active-category");
+
+    // update active category
+    swiperSlide.on("click", () => {
+      subCategoriesSwiperWrapper.empty();
+
+      $(".swiper-slide").removeClass("active-category");
+      swiperSlide.addClass("active-category");
+
+      // check category for subCategory
+      if (data.hasOwnProperty("subCategory")) {
+        data.subCategory.map((subCategory) => {
+          const subCategorySwiperSlide = $("<div>").addClass("swiper-slide");
+          const subCategorySpan = $("<span>").text(subCategory);
+
+          subCategorySwiperSlide.append(subCategorySpan);
+          subCategoriesSwiperWrapper.append(subCategorySwiperSlide);
+        });
+
+        console.log("Has subCategory:", data.subCategory);
+      } else {
+        console.log("Does not have subCategory");
+      }
+    });
 
     swiperSlide.append(category);
     swiperWrapper.append(swiperSlide);
