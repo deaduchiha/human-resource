00541a701cf284f545affbd990c82eabe480d3b7
Alireza Nikzad <alireza.nikzdd@gmail.com>
2023-09-07 12:14:16 +0330

    update code and clean extra codes

---
+++ rinho/Dalia v2.js
@@ -0,0 +1,116 @@
+const categories = [
+  { category: "پیتزا", image: "../rhino templates/fake data/dalia/pizza.png" },
+  { category: "برگر", image: "../rhino templates/fake data/dalia/burger.jpg" },
+  {
+    category: "مرغ سوخاری",
+    image: "../rhino templates/fake data/dalia/pizza.png",
+  },
+  { category: "سالاد", image: "../rhino templates/fake data/dalia/burger.jpg" },
+  {
+    category: "پیش غذا",
+    image: "../rhino templates/fake data/dalia/pizza.png",
+  },
+];
+
+const foods = [
+  {
+    id: 1,
+    category: "پیتزا",
+    images: [
+      "../rhino templates/fake data/gerdoo/1.png",
+      "../rhino templates/fake data/gerdoo/2.png",
+      "../rhino templates/fake data/gerdoo/3.png",
+      "../rhino templates/fake data/gerdoo/4.png",
+    ],
+    title: "مخصوص فرسکو",
+    price: "12000",
+    details:
+      "سس مارینارا، پنیر موتزارلا، پنیر پارمزان، روغن زیتون فرابکر، قارچ، چیپس کدو سبز، بادمجان کبابی، بازیل",
+  },
+  {
+    id: 2,
+    category: "پیتزا",
+    images: [
+      "../rhino templates/fake data/gerdoo/1.png",
+      "../rhino templates/fake data/gerdoo/2.png",
+      "../rhino templates/fake data/gerdoo/3.png",
+      "../rhino templates/fake data/gerdoo/4.png",
+    ],
+    title: "سیر و استیک",
+    price: "13000",
+    details:
+      "سس مارینارا، پنیر موتزارلا، پنیر پارمزان، روغن زیتون فرابکر، قارچ، چیپس کدو سبز، بادمجان کبابی، بازیل",
+  },
+  {
+    id: 3,
+    category: "پیتزا",
+    images: [
+      "../rhino templates/fake data/gerdoo/1.png",
+      "../rhino templates/fake data/gerdoo/2.png",
+      "../rhino templates/fake data/gerdoo/3.png",
+      "../rhino templates/fake data/gerdoo/4.png",
+    ],
+    title: "1سبزیجات",
+    price: "14000",
+    details:
+      "سس مارینارا، پنیر موتزارلا، پنیر پارمزان، روغن زیتون فرابکر، قارچ، چیپس کدو سبز، بادمجان کبابی، بازیل",
+  },
+  {
+    id: 4,
+    category: "پیتزا",
+    images: [
+      "../rhino templates/fake data/gerdoo/1.png",
+      "../rhino templates/fake data/gerdoo/2.png",
+      "../rhino templates/fake data/gerdoo/3.png",
+      "../rhino templates/fake data/gerdoo/4.png",
+    ],
+    title: "2سبزیجات",
+    price: "15000",
+    details:
+      "سس مارینارا، پنیر موتزارلا، پنیر پارمزان، روغن زیتون فرابکر، قارچ، چیپس کدو سبز، بادمجان کبابی، بازیل",
+  },
+  {
+    id: 5,
+    category: "پیتزا",
+    images: [
+      "../rhino templates/fake data/gerdoo/1.png",
+      "../rhino templates/fake data/gerdoo/2.png",
+      "../rhino templates/fake data/gerdoo/3.png",
+      "../rhino templates/fake data/gerdoo/4.png",
+    ],
+    title: "3سبزیجات",
+    price: "16000",
+    details:
+      "سس مارینارا، پنیر موتزارلا، پنیر پارمزان، روغن زیتون فرابکر، قارچ، چیپس کدو سبز، بادمجان کبابی، بازیل",
+  },
+  {
+    id: 6,
+    category: "پیتزا",
+    images: [
+      "../rhino templates/fake data/gerdoo/1.png",
+      "../rhino templates/fake data/gerdoo/2.png",
+      "../rhino templates/fake data/gerdoo/3.png",
+      "../rhino templates/fake data/gerdoo/4.png",
+    ],
+    title: "4سبزیجات",
+    price: "17000",
+    details:
+      "سس مارینارا، پنیر موتزارلا، پنیر پارمزان، روغن زیتون فرابکر، قارچ، چیپس کدو سبز، بادمجان کبابی، بازیل",
+  },
+  {
+    id: 7,
+    category: "برگر",
+    images: [
+      "../rhino templates/fake data/gerdoo/1.png",
+      "../rhino templates/fake data/gerdoo/2.png",
+      "../rhino templates/fake data/gerdoo/3.png",
+      "../rhino templates/fake data/gerdoo/4.png",
+    ],
+    title: "برگر ذغالی",
+    price: "18000",
+    details:
+      "سس مارینارا، پنیر موتزارلا، پنیر پارمزان، روغن زیتون فرابکر، قارچ، چیپس کدو سبز، بادمجان کبابی، بازیل",
+  },
+];
+
+export { categories, foods };
