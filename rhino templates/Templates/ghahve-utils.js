ebb044ccd4e356da36ce530d9588f424fb49760b
Alireza Nikzad <alireza.nikzdd@gmail.com>
2023-09-22 18:10:55 +0330

    update popup and getting images and title and sizes from data

--- rhino templates/Templates/ghahve-utils.js
+++ rhino templates/Templates/ghahve-utils.js
@@ -1,4 +1,28 @@
 export function openPopup(food) {
-  console.log(food);
+  $(".popup_titles h2").text(food.title);
+  $(".popup_titles h3").text(food.englishTitle);
+  $(".popup_price span").text(food.sizes[0].price);
+
+  //   sizes
+  const sizes = $(".popup_sizes");
+  sizes.empty();
+  food.sizes.map((sizeData) => {
+    const size = $("<div>").text(sizeData.size);
+    sizes.append(size);
+  });
+
+  // images
+  const imagesWrapper = $(".popup-images-swiper .swiper-wrapper");
+  imagesWrapper.empty();
+  food.images.map((image) => {
+    const imgSlide = $("<div>").addClass("swiper-slide");
+    const img = $("<img>").attr({
+      src: image,
+      alt: food.title,
+    });
+    imgSlide.append(img);
+    imagesWrapper.append(imgSlide);
+  });
+  //   details
   $("#popup").fadeIn();
 }
