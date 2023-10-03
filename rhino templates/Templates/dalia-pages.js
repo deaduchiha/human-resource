baae6c0b9304b543be337530a95fe85c255026d8
Alireza Nikzad <alireza.nikzdd@gmail.com>
2023-10-03 11:13:18 +0330

    update code

--- rhino templates/Templates/dalia-pages.js
+++ rhino templates/Templates/dalia-pages.js
@@ -20,7 +20,13 @@ new Swiper(".imagesSwiper", {
 });
 
 $(document).ready(function () {
-  const outerSwiper = new Swiper(".mySwiper", {});
+  const outerSwiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".first-page__next",
+      prevEl: ".second-page__prev",
+    },
+    allowTouchMove: false, // Disable dragging on touch devices
+  });
 
   const innerSwiper = new Swiper(".menuSwiper", {
     initialSlide: 0,
